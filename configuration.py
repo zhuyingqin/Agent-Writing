13683f6f4b6d10dfcb063f91b355fce874dadb1b
Lance Martin <lance@langchain.dev>
2024-12-03 16:45:18 -0800

    Add default schema

--- configuration.py
+++ configuration.py
@@ -6,10 +6,24 @@ from langchain_core.runnables import RunnableConfig
 from typing_extensions import Annotated
 from dataclasses import dataclass
 
+DEFAULT_REPORT_STRUCTURE = """The report structure should focus on breaking-down the user-provided topic:
+
+1. Introduction (no research needed)
+   - Brief overview of the topic area
+
+2. Main Body Sections:
+   - Each section should focus on a sub-topic of the user-provided topic
+   - Include any key concepts and definitions
+   - Provide real-world examples or case studies where applicable
+   
+3. Conclusion
+   - Aim for 1 structural element (either a list of table) that distills the main body sections 
+   - Provide a concise summary of the report"""
+
 @dataclass(kw_only=True)
 class Configuration:
     """The configurable fields for the chatbot."""
-    report_structure: str = "Create a report with 5 total sections: Introduction, Conclusion, and 3 sections of research based upon the user topic."
+    report_structure: str = DEFAULT_REPORT_STRUCTURE
     number_of_queries: int = 2 
     tavily_topic: str = "general"
     tavily_days: str = None
