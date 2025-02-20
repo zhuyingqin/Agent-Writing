b2bbdaf6b40ded885707cd008bc6672910d0ba30
moritalous <moritalous@gmail.com>
2025-02-20 10:55:19 +0000

    fix PlannerProvider

--- src/open_deep_research/configuration.py
+++ src/open_deep_research/configuration.py
@@ -35,7 +35,7 @@ class Configuration:
     report_structure: str = DEFAULT_REPORT_STRUCTURE # Defaults to the default report structure
     number_of_queries: int = 2 # Number of search queries to generate per iteration
     max_search_depth: int = 2 # Maximum number of reflection + search iterations
-    planner_provider: PlannerProvider = PlannerProvider.ANTHROPIC # Defaults to OpenAI as provider
+    planner_provider: PlannerProvider = PlannerProvider.OPENAI  # Defaults to OpenAI as provider
     planner_model: str = "o3-mini" # Defaults to OpenAI o3-mini as planner model
     writer_provider: WriterProvider = WriterProvider.ANTHROPIC # Defaults to Anthropic as provider
     writer_model: str = "claude-3-5-sonnet-latest" # Defaults to Anthropic as provider
